b44aa265f80145bb788e9e7d82a471b890559504
Dmitrii Rashchenko <dimitree54@gmail.com>
2024-01-20 11:45:29 +0200

    Refactor github event handler
    Refactor the github_event_handler.py file to improve code maintainability. This includes removing the '_handle_pull_request_event' method and replacing its functionality with 'is_review_requested' and 'get_pull_request' methods. Logger statements have also been adjusted for better logging output.

--- github_event_handler.py
+++ github_event_handler.py
@@ -1,7 +1,7 @@
 from logging import Logger
 from typing import Dict
 
-from github import Github
+from github import Github, PullRequest
 from pydantic import BaseModel
 
 
@@ -13,32 +13,24 @@ class GithubEventHandler(BaseModel):
         self.logger.debug("Event")
         if event == 'issue_comment':
             self._handle_issue_comment_event(payload)
-        elif event == 'pull_request':
-            self._handle_pull_request_event(payload)
+
+    def is_review_requested(self, payload: Dict) -> bool:
+        pass
+
+    def get_pull_request(self, payload: Dict) -> PullRequest:
+        repo_name = payload['repository']['full_name']
+        pull_request_number = payload['pull_request']['number']
+        repo = self.github.get_repo(repo_name)
+        pull_request = repo.get_pull(pull_request_number)
+        return pull_request
 
     def _handle_issue_comment_event(self, payload):
         self.logger.debug("IssueComment")
         self.logger.debug("payload: {}".format(payload))
 
-        if 'comment' in payload and '@ai-code-reviewer-bot' in payload['comment']['body']:
-            self.logger.debug("Payload valid")
-            repo_name = payload['repository']['full_name']
-            issue_number = payload['issue']['number']
-            repo = self.github.get_repo(repo_name)
-            issue = repo.get_issue(number=issue_number)
-            issue.create_comment("Did you call me?")
-
-    def _handle_pull_request_event(self, payload):
-        self.logger.debug("PullRequest")
-        self.logger.debug("payload: {}".format(payload))
-
-        if (payload['action'] in ['opened', 'synchronize'] and
-                '@ai-code-reviewer-bot' in payload['pull_request']['body']):
-            self.logger.debug("Payload valid")
-            repo_name = payload['repository']['full_name']
-            pr_number = payload['pull_request']['number']
-            repo = self.github.get_repo(repo_name)
-            pr = repo.get_pull(pr_number)
-            files = pr.get_files()
+        if self.is_review_requested(payload):
+            self.logger.debug("Review requested")
+            pull_request = self.get_pull_request(payload)
+            files = pull_request.get_files()
             for file in files:
-                print(f"File: {file.filename}, Changes: {file.patch}")
+                self.logger.debug(f"File: {file.filename}, Changes: {file.patch}")
